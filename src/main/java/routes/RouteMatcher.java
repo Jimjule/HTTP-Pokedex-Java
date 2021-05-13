ea04d24e8a6695b5f575ba1071a99632ec22e516
Jimjule <roadknightj@gmail.com>
2021-05-13 14:46:48 +0100

    Adds pokemon JSON responses

--- src/main/java/routes/RouteMatcher.java
+++ src/main/java/routes/RouteMatcher.java
@@ -2,6 +2,9 @@ package routes;
 
 import route.Route;
 import routes.files.HealthCheckHTMLRoute;
+import routes.structured.text.BulbasaurResponse;
+import routes.structured.text.CharmanderResponse;
+import routes.structured.text.SquirtleResponse;
 
 public class RouteMatcher {
     public static Route getRoute(String path) {
@@ -10,6 +13,15 @@ public class RouteMatcher {
             switch (path) {
                 case "/health-check.html":
                     route = new HealthCheckHTMLRoute();
+                    break;
+                case "/pokemon/id/1":
+                    route = new BulbasaurResponse();
+                    break;
+                case "/pokemon/id/4":
+                    route = new CharmanderResponse();
+                    break;
+                case "/pokemon/id/7":
+                    route = new SquirtleResponse();
                     break;
                 default:
                     break;
