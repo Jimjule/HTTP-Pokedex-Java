fd9cd650953b6c83f181ddb2aa66de09c2349331
Jimjule <roadknightj@gmail.com>
2021-05-17 09:58:55 +0100

    Uses port 5001

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import java.net.ServerSocket;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int port = 5000;
+        int port = 5001;
         ServerSocket serverSocket = new ServerSocket(port);
         HTTPServer httpServer = new HTTPServer(serverSocket);
         httpServer.start();
